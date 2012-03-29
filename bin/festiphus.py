adc42fce43af53b911bffd361230e799f00013f7
don <drowe2@lion.lmu.edu>
2012-03-29 01:55:22 -0700

    Added a scrollbar to the remote file browser, refactored names, and added labels to the inputs.

--- bin/festiphus.py
+++ bin/festiphus.py
@@ -12,9 +12,12 @@ SERVER_IP = '127.0.0.1'
 SERVER_PORT = 3001
 
 #GUI Grid constants:
-CLIENT_ROW = 0
-SERVER_ROW = 1
-BROWSER_ROW = 2
+CLIENT_LABEL_ROW = 0
+CLIENT_ROW = 1
+SERVER_LABEL_ROW = 2
+SERVER_ROW = 3
+BROWSER_LABEL_ROW = 4
+BROWSER_ROW = 5
 
 #instance for a Festiphus session:
 class Festiphus(Frame):
@@ -33,7 +36,7 @@ class Festiphus(Frame):
         new_conn.connect(host, port)
         new_conn.login(name, password)
         self.sessions.append(new_conn)
-        self.refresh_file_browser(new_conn)
+        self.refresh_remote_browser(new_conn)
         
         #for right now, the active session is just this one:
         self.current_session = new_conn
@@ -47,20 +50,20 @@ class Festiphus(Frame):
         self.open_connection(host, port, name, password)
 
     #Refresh the file browser and the directory name:
-    def refresh_file_browser(self, conn):
+    def refresh_remote_browser(self, conn):
         #Set the new current directory:
-        self.current_dir.set(conn.pwd())
+        self.current_remote_dir.set(conn.pwd())
         
         #clear out the current list:
-        self.file_browser.delete(0, END)
+        self.remote_browser.delete(0, END)
 
         #display the directory contents:
         self.file_list = conn.nlst() #debating whether this should be in class
         #if this isn't the root dir, put a dir link to up one level:
         if conn.pwd() != '/':
-            self.file_browser.insert(0, '..')
+            self.remote_browser.insert(0, '..')
         for f in self.file_list:
-            self.file_browser.insert(END, f)
+            self.remote_browser.insert(END, f)
         
     #Move the working directory to the given dir in the given connection:
     def cd(self, directory, conn):
@@ -68,16 +71,16 @@ class Festiphus(Frame):
         conn.cwd(directory)
         
         #refresh the file browser:
-        self.refresh_file_browser(conn)
+        self.refresh_remote_browser(conn)
     
     #Use the input to change the directory:
-    def submit_directory(self):
+    def submit_remote_directory(self):
         #A list of the indices that are currently selected in the file browser:
-        cur_selected = self.file_browser.curselection()
+        cur_selected = self.remote_browser.curselection()
         
         #The relative name of the first selected item:
         #TODO: make sure it's a dir and not a file
-        target_dir = self.file_browser.get(cur_selected[0])
+        target_dir = self.remote_browser.get(cur_selected[0])
         
         #Move to the given dir:
         self.cd(target_dir, self.current_session)
@@ -112,23 +115,35 @@ class Festiphus(Frame):
     ################GUI######################
     #Build the widgets
     def createWidgets(self):
-
+        
         ##Client Control
         #host entry:
         self.host_input = Entry(self)
         self.host_input.grid(column = 0, row = CLIENT_ROW)
+        #label:
+        self.host_input_label = Label(self, text = "Host:")
+        self.host_input_label.grid(column = 0, row = CLIENT_LABEL_ROW)
         
         #port entry:
         self.port_input = Entry(self)
         self.port_input.grid(column = 1, row = CLIENT_ROW)
+        #label:
+        self.port_input_label = Label(self, text = "Port:")
+        self.port_input_label.grid(column = 1, row = CLIENT_LABEL_ROW)
 
         #name entry:
         self.name_input = Entry(self)
         self.name_input.grid(column = 2, row = CLIENT_ROW)
+        #label:
+        self.name_input_label = Label(self, text = "Name:")
+        self.name_input_label.grid(column = 2, row = CLIENT_LABEL_ROW)
 
         #password entry:
         self.password_input = Entry(self, show = '*')
         self.password_input.grid(column = 3, row = CLIENT_ROW)
+        #label:
+        self.password_input_label = Label(self, text = "Password:")
+        self.password_input_label.grid(column = 3, row = CLIENT_LABEL_ROW)
         
         #connect button:
         self.connect_button = Button(self, text = 'Connect',
@@ -139,30 +154,50 @@ class Festiphus(Frame):
         #new name entry:
         self.new_name_input = Entry(self)
         self.new_name_input.grid(column = 0, row = SERVER_ROW)
+        #label:
+        self.new_name_input_label = Label(self, text = "New User Name:")
+        self.new_name_input_label.grid(column = 0, row = SERVER_LABEL_ROW)
         
         #new password entry:
         self.new_pass_input = Entry(self)
         self.new_pass_input.grid(column = 1, row = SERVER_ROW)
+        #label:
+        self.new_pass_input_label = Label(self, text = "New User Password:")
+        self.new_pass_input_label.grid(column = 1, row = SERVER_LABEL_ROW)
         
         #add new user button:
         self.new_user_button = Button(self, text = 'Add New User',
                                       command = self.add_user)
         self.new_user_button.grid(column = 2, row = SERVER_ROW)
-
-        #current directory:
-        self.current_dir = StringVar() #dir_label will follow this
-        self.dir_label = Label(self, textvariable = self.current_dir)
-        self.dir_label.grid(row = BROWSER_ROW)
-
-        ##file browser:
-        #window:
-        self.file_browser = Listbox(self)
-        self.file_browser.grid(columnspan = 4, column = 0, row = BROWSER_ROW)
         
-        #change dir button:
-        self.change_dir_button = Button(self, text = 'Change Directory',
-                                        command = self.submit_directory)
-        self.change_dir_button.grid(column = 4, row = BROWSER_ROW)
+
+        ##remote file browser:
+        #scrollbar:
+        self.remote_browser_scrollbar = Scrollbar(self)
+        
+        
+        #window:
+        self.remote_browser = Listbox(self, 
+                                      yscrollcommand = self.remote_browser_scrollbar.set)
+        #self.remote_browser.grid(columnspan = 4, column = 0, row = BROWSER_ROW)
+        self.remote_browser.grid(column = 0, row = BROWSER_ROW)
+        
+        #initialize scrollbar:
+        self.remote_browser_scrollbar.grid(column = 1, row = BROWSER_ROW,
+                                           sticky = N+S+W)
+        self.remote_browser_scrollbar.config(command = self.remote_browser.yview)
+        
+        #current directory label:
+        self.current_remote_dir = StringVar() #remote_dir_label will follow this
+        self.remote_dir_label = Label(self, textvariable = self.current_remote_dir)
+        self.remote_dir_label.grid(row = BROWSER_LABEL_ROW)
+        
+        
+        
+        ##change remote dir button:
+        self.change_remote_dir_button = Button(self, text = 'Change Directory',
+                                        command = self.submit_remote_directory)
+        self.change_remote_dir_button.grid(column = 4, row = BROWSER_ROW)
     
     ###################Fire it up!#################
     def __init__(self, master = None):
