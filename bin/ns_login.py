d5b7e751a41055b74fedf58cfe14fb8c30c437d1
don <drowe2@lion.lmu.edu>
2012-04-16 01:02:17 -0700

    Changed part of top comment in ns_login.py to a docstring

--- bin/ns_login.py
+++ bin/ns_login.py
@@ -1,6 +1,9 @@
 #ns_login.py
 #Festiphus nameserver login dialog class
-#
-#Prompts user for address of nameserver (if other than default)
-#and name to use on given nameserver. Checks nameserver to
-#ensure that the name has not already been taken, too.
+
+"""Prompts user for address of nameserver (if other than default)
+and name to use on given nameserver. Checks nameserver to
+ensure that the name has not already been taken, too.
+"""
+
+
